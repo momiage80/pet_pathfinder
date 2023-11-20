dcbc041e4038098bf761d27695b4e097f1ad9fce
r_nagata <r_nagata@LAPTOP-7J397IBC>
2023-11-20 13:54:17 +0900

    ほげほげ

--- src/Servlet/TopServlet.java
+++ src/Servlet/TopServlet.java
@@ -17,7 +17,6 @@ public class TopServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    	// TODO 自動生成されたメソッド・スタブ
     	RequestDispatcher dispatcher = req.getRequestDispatcher("html/Petpathfinder_Top.html");
     	dispatcher.forward(req, resp);
     }
