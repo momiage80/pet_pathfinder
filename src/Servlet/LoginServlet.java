3289ebe4365159e873b1855972de9aea5e819c11
Oharahogepiyoshur <ksw2270048@stu.o-hara.ac.jp>
2023-11-28 13:35:21 +0900

    コミット

---
+++ src/Servlet/LoginServlet.java
@@ -0,0 +1,28 @@
+package Servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("jsp/login.jsp");
+		dispatcher.forward(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("html/PPF_Mypage.html");
+		dispatcher.forward(req, resp);
+	}
+}
---
+++ src/Servlet/TopServlet.java
@@ -0,0 +1,23 @@
+package Servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TopServlet
+ */
+@WebServlet("/Top")
+public class TopServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	RequestDispatcher dispatcher = req.getRequestDispatcher("html/Petpathfinder_Top.html");
+    	dispatcher.forward(req, resp);
+    }
+}
