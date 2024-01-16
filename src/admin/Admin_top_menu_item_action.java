1a36ac9d7acc3d135fe7607ade7d6b1c8e0c32ab
ritensub <ksw2270292@stu.o-hara.ac.jp>
2024-01-16 14:02:31 +0900

    管理者メニューの画面遷移修正

--- src/admin/Admin_top_menu_item_action.java
+++ src/admin/Admin_top_menu_item_action.java
@@ -4,14 +4,16 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns={"/admin_jsp/item"})
-public class Admin_top_menu_item_action {
+@WebServlet(urlPatterns={"/item"})
+public class Admin_top_menu_item_action extends HttpServlet{
+	private static final long serialVersionUID = 1L;
     public void doGet(
     		HttpServletRequest request, HttpServletResponse response
     ) throws ServletException, IOException{
-    		request.getRequestDispatcher("gift_list.jsp").forward(request, response);
+    		request.getRequestDispatcher("admin_jsp/gift_list.jsp").forward(request, response);
     }
 }
